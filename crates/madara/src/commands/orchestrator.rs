121c01480d492b5710bc86eaf65a35611f77417d
ftheirs <fntheirs@gmail.com>
2025-02-05 17:09:57 -0300

    template for orchestrator config

--- crates/madara/src/commands/orchestrator.rs
+++ crates/madara/src/commands/orchestrator.rs
@@ -10,9 +10,14 @@ use crate::{
     constants::{DEPS_REPO_PATH, MSG_BUILDING_IMAGE_SPINNER},
 };
 
+use minijinja::{context, Environment};
+use std::fs;
+
 const ORCHESTRATOR_REPO_PATH: &str = "deps/orchestrator";
 const ORCHESTRATOR_DOCKER_IMAGE: &str = "orchestrator";
 const ORCHESTRATOR_COMPOSE_FILE: &str = "compose.yaml";
+const ORCHESTRATOR_ENV_TEMPLATE_FILE: &str = ".env.template";
+const ORCHESTRATOR_ENV_FILE: &str = ".env";
 
 pub(crate) fn run(args_madara: MadaraRunnerConfigMode, shell: &Shell) -> anyhow::Result<()> {
     logger::new_empty_line();
@@ -26,7 +31,8 @@ pub(crate) fn run(args_madara: MadaraRunnerConfigMode, shell: &Shell) -> anyhow:
     commands::pathfinder::parse_params(&args_pathfinder)?;
 
     // Collect Prover configuration
-    let _args_prover = ProverRunnerConfig::default().fill_values_with_prompt()?;
+    let args_prover = ProverRunnerConfig::default().fill_values_with_prompt()?;
+    populate_orchestrator_env(&args_prover)?;
 
     // Build all images
     build_images(shell)?;
@@ -59,6 +65,31 @@ fn build_image(shell: &Shell) -> anyhow::Result<()> {
         ORCHESTRATOR_REPO_PATH.to_string(),
         ORCHESTRATOR_DOCKER_IMAGE.to_string(),
     )?;
+
+    Ok(())
+}
+
+fn populate_orchestrator_env(prover_config: &ProverRunnerConfig) -> anyhow::Result<()> {
+    let env_template = format!(
+        "{}/{}",
+        ORCHESTRATOR_REPO_PATH, ORCHESTRATOR_ENV_TEMPLATE_FILE
+    );
+    let env_output = format!("{}/{}", ORCHESTRATOR_REPO_PATH, ORCHESTRATOR_ENV_FILE);
+
+    // Read the template file
+    let template = fs::read_to_string(env_template).expect("Failed to read .env.template");
+
+    // Set up MiniJinja
+    let mut env = Environment::new();
+    env.add_template("env_template", &template)
+        .expect("Failed to add template");
+
+    let data = context! {MADARA_ORCHESTRATOR_ATLANTIC_API_KEY => prover_config.url};
+
+    // Render the template
+    let tmpl = env.get_template("env_template").unwrap();
+    let rendered = tmpl.render(&data).expect("Template rendering failed");
+    fs::write(env_output, rendered).expect("Failed to write .env");
 
     Ok(())
 }
