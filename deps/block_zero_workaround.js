403738c73e506732e4cd7c86b4497a2564978112
ftheirs <fntheirs@gmail.com>
2025-02-26 15:20:59 -0300

    retrieve last madara block and insert state transition until that block

--- deps/block_zero_workaround.js
+++ deps/block_zero_workaround.js
@@ -1,7 +1,47 @@
-// SNOS is unable to process block 0, so we need to manually add an entry to the database to simulate 
-// that block 0 has been settled into L1.
-// This is a necessary step to allow the orchestrator to continue processing blocks, 
+// SNOS is unable to process block 0 ~ X, where X is the block where Bootstrapper setup-l2 finishes.
+// So we need to manually add an entry to the database to simulate
+// that these blocks are settled in the L1.
+// This is a necessary step to allow the orchestrator to continue processing blocks,
 // as it would otherwise become stuck.
+
+async function insertStateTransition() {
+    // Fetch madara last block
+    const LAST_BLOCK_NUMBER = await fetch('http://madara:9945/v0_7_1/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            "jsonrpc": "2.0",
+            "method": "starknet_blockNumber",
+            "params": [],
+            "id": 1
+        }),
+    })
+        .then(response => response.json())
+        .then(data => data.result);
+
+    console.log("Madara last block: {}", LAST_BLOCK_NUMBER)
+
+    for (let i = 0; i <= LAST_BLOCK_NUMBER; i++) {
+        db.jobs.insertOne({
+            _id: ObjectId(),
+            internal_id: i.toString(),
+            job_type: "StateTransition",
+            created_at: ISODate(`2024-12-06T10:07:05.000Z`),
+            external_id: i.toString(),
+            id: BinData(4, `H1c+V6J/TCaCID2LwJ5e/g==0`),
+            metadata: {
+                attempt_tx_hashes_0: `0x0adca7145e618564bc5ec901b80d331e11e3207ac21e68c4cedb698ff5ce6cb0`,
+                process_attempt_no: "1",
+                blocks_number_to_settle: i.toString()
+            },
+            status: "Completed",
+            updated_at: ISODate(`2024-12-06T10:18:20.000Z`),
+            version: 3
+        });
+    }
+}
 
 db = connect("mongodb://mongodb:27017/orchestrator");
 
@@ -10,19 +50,5 @@ db = connect("mongodb://mongodb:27017/orchestrator");
 // and we don't need to manually delete the entries from MongoDB
 db.jobs.deleteMany({});
 
-db.jobs.insertOne({
-    _id: ObjectId("6752ccc937d965f72eff206d"),
-    internal_id: "0",
-    job_type: "StateTransition",
-    created_at: ISODate("2024-12-06T10:07:05.000Z"),
-    external_id: "0",
-    id: BinData(4, "H1c+V6J/TCaCID2LwJ5e/g=="),
-    metadata: {
-        attempt_tx_hashes_0: "0x0adca7145e618564bc5ec901b80d331e11e3207ac21e68c4cedb698ff5ce6cb0",
-        process_attempt_no: "1",
-        blocks_number_to_settle: "0"
-    },
-    status: "Completed",
-    updated_at: ISODate("2024-12-06T10:18:20.000Z"),
-    version: 3
-});
+// Insert state transition completed until last Madara block
+insertStateTransition();
